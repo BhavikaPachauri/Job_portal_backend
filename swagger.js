d66c255cce4c22ac4fe0f5feaf35a808caaa3ad5
BhavikaPachauri <pachauribhavika2000@gmail.com>
2025-07-04 10:55:47 +0530

    first commit

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: 'https://job-portal-backend-d1le.onrender.com',
       },
     ],
     components: {
